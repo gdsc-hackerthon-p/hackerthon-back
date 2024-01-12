86dcc68d0c378aabbf8d2401e766a2cb9cf81c44
Ahn Jiwan <synoti21@naver.com>
2024-01-12 19:39:09 +0900

    :bug: fix: 특정 날짜에 해당하는 모든 객체 불러오기 (#6)

--- src/main/java/com/gdsc/hackerthon/check/repository/CheckRepository.java
+++ src/main/java/com/gdsc/hackerthon/check/repository/CheckRepository.java
@@ -1,2 +1,15 @@
-package com.gdsc.hackerthon.check.repository;public interface CheckRepository {
+package com.gdsc.hackerthon.check.repository;
+
+import com.gdsc.hackerthon.check.domain.Check;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+
+public interface CheckRepository extends JpaRepository<Check, Long> {
+    @Query("SELECT c FROM check c WHERE c.checkDate >= :startOfDay AND c.checkDate < :endOfDay ORDER BY c.checkDate DESC")
+    Page<Check> findByCheckDateBetweenOrderByCheckDateDesc(Pageable pageable, @Param("startOfDay") LocalDateTime startOfDay, @Param("endOfDay") LocalDateTime endOfDay);
 }
