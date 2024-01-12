e21dbac6dafd013fee37fe8c15f2a88865b5ad51
Ahn Jiwan <synoti21@naver.com>
2024-01-13 05:25:10 +0900

    :ambulance: hotfix: github token 환경변수로 저장 (#19)

--- src/main/java/com/gdsc/hackerthon/github/application/GithubService.java
+++ src/main/java/com/gdsc/hackerthon/github/application/GithubService.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gdsc.hackerthon.util.api.ResponseCode;
 import com.gdsc.hackerthon.util.exception.UserException;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.kohsuke.github.*;
 import org.springframework.stereotype.Service;
@@ -25,8 +26,7 @@ public class GithubService {
 
     public GHUser getGithubUser(String githubId) throws IOException {
         try{
-            GitHub github = new GitHubBuilder().withOAuthToken("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").build();
-            log.info("githubId: {}", githubId);
+            GitHub github = new GitHubBuilder().withOAuthToken(System.getenv("GITHUB_TOKEN")).build();
             return github.getUser(githubId);
         }catch (IOException e) {
             throw new UserException(ResponseCode.GITHUB_USER_NOT_FOUND);
@@ -37,7 +37,7 @@ public class GithubService {
         Calendar calendar = Calendar.getInstance();
         Set<Date> commitDates = new HashSet<>();
 
-        GitHub github = new GitHubBuilder().withOAuthToken("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").build();
+        GitHub github = new GitHubBuilder().withOAuthToken(System.getenv("GITHUB_TOKEN")).build();
         int count = 0;
         //오늘부터 날짜를 거슬러 올라오면서 커밋이 한개라도 있으면 다음 날짜로 넘어가고, 커밋이 없으면 멈춤
         while (true) {
