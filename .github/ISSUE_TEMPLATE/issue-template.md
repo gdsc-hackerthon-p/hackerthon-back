1603349bf5ea63c58c98a9f43450e0d632d46a0d
Ahn Jiwan <synoti21@naver.com>
2024-01-12 15:23:35 +0900

    :tada: feat: initial commit

---
+++ .github/ISSUE_TEMPLATE/issue-template.md
@@ -0,0 +1,16 @@
+---
+name: Issue Template
+about: Basic Template when publishing issues
+title: ''
+labels: ''
+assignees: synoti21
+
+---
+
+### Issue 요약
+
+-
+
+### 상세내용
+
+-
---
+++ src/main/java/com/gdsc/hackerthon/HackerthonApplication.java
@@ -0,0 +1,13 @@
+package com.gdsc.hackerthon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackerthonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackerthonApplication.class, args);
+	}
+
+}
